122278639ca2d7d0ae1ec06dc33bffbe6997a9b1
m khairul ramadhan <khairulr59@gmail.com>
2021-05-26 22:58:11 +0700

    done create all base file

--- Android Development/LunionApp/app/src/main/java/com/lunion/lunionapp/MainActivity.kt
+++ Android Development/LunionApp/app/src/main/java/com/lunion/lunionapp/MainActivity.kt
@@ -2,10 +2,50 @@ package com.lunion.lunionapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.Fragment
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import com.lunion.lunionapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var viewBinding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        viewBinding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(viewBinding.root)
+
+        viewBinding.bottomNavigationView.setOnNavigationItemSelectedListener(onNavSelect)
+
+        //default select fragment
+        val fragment = DetectionFragment.newInstance()
+        addFragment(fragment)
     }
+
+    private val onNavSelect = BottomNavigationView.OnNavigationItemSelectedListener { item ->
+        when (item.itemId) {
+            R.id.nav_detection -> {
+                val fragment = DetectionFragment.newInstance()
+                addFragment(fragment)
+                return@OnNavigationItemSelectedListener true
+            }
+            R.id.nav_news -> {
+                val fragment = NewsFragment()
+                addFragment(fragment)
+                return@OnNavigationItemSelectedListener true
+            }
+            R.id.nav_air_quality -> {
+                val fragment = AirQualityFragment()
+                addFragment(fragment)
+                return@OnNavigationItemSelectedListener true
+            }
+        }
+        false
+    }
+
+    private fun addFragment(fragment: Fragment) {
+        supportFragmentManager
+            .beginTransaction()
+            .replace(R.id.layout_container, fragment, fragment.javaClass.simpleName)
+            .commit()
+    }
+
 }
