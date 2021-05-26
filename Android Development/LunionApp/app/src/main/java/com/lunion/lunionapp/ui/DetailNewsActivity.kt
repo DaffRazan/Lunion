d90c8849360d0355f2b692c2668e05abaccacbc3
Daffa Razan <daffarazan17@gmail.com>
2021-05-27 01:55:34 +0700

    Update UI improvements 1.0

--- Android Development/LunionApp/app/src/main/java/com/lunion/lunionapp/ui/DetailNewsActivity.kt
+++ Android Development/LunionApp/app/src/main/java/com/lunion/lunionapp/ui/DetailNewsActivity.kt
@@ -1,11 +1,12 @@
-package com.lunion.lunionapp
+package com.lunion.lunionapp.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.lunion.lunionapp.R
 
-class ResultDetectionActivity : AppCompatActivity() {
+class DetailNewsActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_result_detection)
+        setContentView(R.layout.activity_detail_news)
     }
 }
--- Android Development/LunionApp/app/src/main/java/com/lunion/lunionapp/ui/NewsFragment.kt
+++ Android Development/LunionApp/app/src/main/java/com/lunion/lunionapp/ui/NewsFragment.kt
@@ -1,10 +1,11 @@
-package com.lunion.lunionapp
+package com.lunion.lunionapp.ui
 
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.lunion.lunionapp.R
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -13,10 +14,10 @@ private const val ARG_PARAM2 = "param2"
 
 /**
  * A simple [Fragment] subclass.
- * Use the [AirQualityFragment.newInstance] factory method to
+ * Use the [NewsFragment.newInstance] factory method to
  * create an instance of this fragment.
  */
-class AirQualityFragment : Fragment() {
+class NewsFragment : Fragment() {
     // TODO: Rename and change types of parameters
     private var param1: String? = null
     private var param2: String? = null
@@ -34,7 +35,7 @@ class AirQualityFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_air_quality, container, false)
+        return inflater.inflate(R.layout.fragment_news, container, false)
     }
 
     companion object {
@@ -44,12 +45,12 @@ class AirQualityFragment : Fragment() {
          *
          * @param param1 Parameter 1.
          * @param param2 Parameter 2.
-         * @return A new instance of fragment AirQualityFragment.
+         * @return A new instance of fragment NewsFragment.
          */
         // TODO: Rename and change types and number of parameters
         @JvmStatic
         fun newInstance(param1: String, param2: String) =
-            AirQualityFragment().apply {
+            NewsFragment().apply {
                 arguments = Bundle().apply {
                     putString(ARG_PARAM1, param1)
                     putString(ARG_PARAM2, param2)
