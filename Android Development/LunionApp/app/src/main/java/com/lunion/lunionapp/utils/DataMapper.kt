774c9b28258e865c36b8f0eab9def6e4276d7bff
Daffa Razan <daffarazan17@gmail.com>
2021-05-30 08:59:34 +0700

    Add splash screen, icon app and update some UIs

--- Android Development/LunionApp/app/src/main/java/com/lunion/lunionapp/utils/DataMapper.kt
+++ Android Development/LunionApp/app/src/main/java/com/lunion/lunionapp/utils/DataMapper.kt
@@ -3,16 +3,17 @@ package com.lunion.lunionapp.utils
 import com.lunion.lunionapp.data.response.news.Article
 import com.lunion.lunionapp.model.NewsModel
 
-object DataMapper{
-    fun mapArticleToNewsModel(data: Article): NewsModel{
+object DataMapper {
+    fun mapArticleToNewsModel(data: Article): NewsModel {
         return NewsModel(
-                author = data.author,
+            author = data.author,
             content = data.content,
             description = data.description,
             publishedAt = data.publishedAt,
             source = data.source.name,
             title = data.title,
             url = data.url,
-            urlToImage = data.urlToImage)
+            urlToImage = data.urlToImage
+        )
     }
 }
